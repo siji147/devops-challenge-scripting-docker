1651bd6be3f4e6e56b4d1cc1ed7c433d97628d6e
siji147 <sijiajayi1@gmail.com>
2022-02-20 23:42:06 +0200

    modify mkdockerize.sh to archive static files not all files

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 input_value=$1
 
-if [ $input_value == serve ]
-then
-    echo "now serving documents..."
-    mkdocs serve
-elif [ $input_value == produce ]
+if [ $input_value == produce ]
 then
     echo "now packaging documents..."
+    mkdocs build
+    echo "zipping mkdocs resources..."
+    # archive the static assests produced in ./site directory
+    tar -czf mkdocs-files.tar.gz ./site
+    # archive all files in the mkdocs root directory
+    # tar -czf mkdocs-files.tar.gz .
+elif [ $input_value == serve ]
+then
+    echo "extracting static files from archive..."
+    tar -xvzf mkdocs-files.tar.gz
+    echo "now serving documents..."
+    mkdocs serve -a localhost:8000
 else
     echo "Invalid command entered. Valid commands are 'serve' and 'produce'"
 fi
